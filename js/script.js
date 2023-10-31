3b6177f22fe2d14827fe74a8920ece1c532b9f60
matteobonelli <matteobonelli08@gmail.com>
2023-10-31 16:14:01 +0100

    can't read

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return{
+        tasks : [{
+                text : 'Pulire camera',
+                done : false,
+                id : 1
+            },
+            {
+                text : 'Lavare i vetri',
+                done : false,
+                id : 2
+            },
+            {
+                text : 'Rifare il letto',
+                done : true,
+                id : 3
+            },
+            {
+                text : 'Stendere i panni',
+                done : false,
+                id : 4
+            }
+            ]
+            
+        }
+    },
+    methods : {
+
+    }
+}).mount('#app')
