d9702f030ef8785a8a28980d4919a84b3278ed7a
matteobonelli <matteobonelli08@gmail.com>
2023-10-31 16:44:56 +0100

    milestone 3 completed

--- js/script.js
+++ js/script.js
@@ -3,33 +3,44 @@ const { createApp } = Vue;
 createApp({
     data() {
         return{
-        tasks : [{
-                text : 'Pulire camera',
-                done : false,
-                id : 1
-            },
-            {
-                text : 'Lavare i vetri',
-                done : false,
-                id : 2
-            },
-            {
-                text : 'Rifare il letto',
-                done : true,
-                id : 3
-            },
-            {
-                text : 'Stendere i panni',
-                done : false,
-                id : 4
-            }
-            ]
-            
+            tasks : [{
+                    text : 'Pulire camera',
+                    done : false,
+                    id : 1
+                },
+                {
+                    text : 'Lavare i vetri',
+                    done : false,
+                    id : 2
+                },
+                {
+                    text : 'Rifare il letto',
+                    done : true,
+                    id : 3
+                },
+                {
+                    text : 'Stendere i panni',
+                    done : false,
+                    id : 4
+                }
+                ],
+            idLast : 4 ,
+            inputText : ''            
         }
     },
     methods : {
         removeItem(index){
             this.tasks.splice(index, 1);
+        },
+        newTask(){
+            this.idLast++
+            const newObj = {
+                text : this.inputText,
+                done : false,
+                id : this.idLast
+            }
+            this.tasks.unshift(newObj);
+            this.inputText = ''
         }
     }
 }).mount('#app')
